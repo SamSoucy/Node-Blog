ba3c0c99d00b003cb7b0124d57b2a613ea838150
Samsoucy <newf930@outlook.com>
2019-02-20 18:24:36 -0500

    added get to return a post and a user with id

--- users/user-router.js
+++ users/user-router.js
@@ -14,6 +14,23 @@ router.get('/', async (req, res) => {
         error: 'The users information could not be retrieved.',
       });
     }
+});
+  
+router.get('/:id', async (req, res) => {
+    try {
+      const user = await Users.getById(req.params.id);
+  
+      if (user.length !== 0) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist."  });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "The user information could not be retrieved.",
+      });
+    }
   });
   
   module.exports = router;
