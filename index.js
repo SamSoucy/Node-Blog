98ab96ff54a72b4bf22514b9dc74fc2f09ee21e5
Samsoucy <newf930@outlook.com>
2019-02-21 15:20:32 -0500

    try 2

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const server = require("./server");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log('\n* Server Running on ${port} *\n');
-})
+});
